cbad94f79f1be9ebb2e8e3e5a3009573ea90bd2e
guizaramellaf91 <guizaramellaf91@gmail.com>
2021-10-24 13:13:11 -0300

    change cors

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
