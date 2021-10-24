3e2f548990bf8b5e8c8742781550686a3d549641
guizaramellaf91 <guizaramellaf91@gmail.com>
2021-10-24 15:11:11 -0300

    up changes

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-var corsOptions = { origin: "http://localhost:8780/" };
-
 app.use(cors()).use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
